0b38c4a470996d8ab47a93adfa8e53e870d4690d
LittleSheep <littlesheep.code@hotmail.com>
2024-02-14 15:06:51 +0800

    :sparkles: Expand config search area

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,6 @@ COPY . .
 ENV RUSTFLAGS="-C target-feature=-crt-static"
 RUN cargo build --release
 
-# Runtime
-FROM alpine:latest
-
-COPY --from=roadsign-server /source/target/release/roadsign /roadsign/server
-
 EXPOSE 81
 
-CMD ["/roadsign/server"]
+CMD ["/source/target/release/roadsign"]
--- src/config/loader.rs
+++ src/config/loader.rs
@@ -3,6 +3,7 @@ use config::Config;
 pub fn load_settings() -> Config {
     Config::builder()
         .add_source(config::File::with_name("Settings"))
+        .add_source(config::File::with_name("/Settings"))
         .add_source(config::Environment::with_prefix("ROADSIGN"))
         .build()
         .unwrap()
