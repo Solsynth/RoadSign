5eef06bd42d71cd005f5581178f5bbc9c538bbb2
LittleSheep <littlesheep.code@hotmail.com>
2025-01-14 18:06:01 +0800

    :test_tube: Testing SSR with nextjs

--- pkg/navi/warden.go
+++ pkg/navi/warden.go
@@ -1,9 +1,14 @@
 package navi
 
-import "git.solsynth.dev/goatworks/roadsign/pkg/warden"
+import (
+	"git.solsynth.dev/goatworks/roadsign/pkg/warden"
+	"github.com/rs/zerolog/log"
+)
 
 func InitializeWarden(regions []*Region) {
 	pool := make([]*warden.AppInstance, 0)
+
+	log.Info().Msg("Starting Warden applications...")
 
 	for _, region := range regions {
 		for _, application := range region.Applications {
@@ -15,5 +20,7 @@ func InitializeWarden(regions []*Region) {
 
 	// Hot swap
 	warden.InstancePool = pool
-	warden.StartPool()
+	errs := warden.StartPool()
+
+	log.Info().Any("errs", errs).Msg("Warden applications has been started.")
 }
